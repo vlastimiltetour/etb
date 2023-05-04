ddb2f2c0b078e5d1236fd80636e67142d9c937d6
vlastimil.tetour <v.tetour@gmail.com>
2023-05-04 16:46:55 +0200

    columns repaired

--- efir/catalog/views.py
+++ efir/catalog/views.py
@@ -1,14 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Category, Product
 
 # Create your views here.
-def home(request):
-    return render(request, "catalog/home.html")
 
 
+# returns home landing page
+def home(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(
+        request,
+        "catalog/home.html",
+        {"category": category, "categories": categories, "products": products},
+    )
+
+
+# returns product list
 def catalog_product_list(request, category_slug=None):
-    return render(request, "catalog/catalog.html")
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(
+        request,
+        "catalog/catalog.html",
+        {"category": category, "categories": categories, "products": products},
+    )
 
 
 def product_detail(request, id, slug):
